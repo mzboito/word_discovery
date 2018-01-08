dd6d5d52818e261a9483504c281043975ed78f58
Marcely Zanon Boito <mzboito@gmail.com>
2018-01-08 16:44:14 +0100

    fix mergedMatrices

--- matrices/mergeMatrices.py
+++ matrices/mergeMatrices.py
@@ -85,6 +85,7 @@ def find_matrices(path_folders, folders, files_dict, file_name):
 	else:
 		size = len(matrices[0])
 		for i in range(1, len(folders)):
+			print matrices[0][0]
 			print matrices[i][0]
 			if len(matrices[i]) != size:
 				print "PROBLEM WITH MATRIX SIZE: " + file_name + " folder: " + str(i+1) + "\n"
