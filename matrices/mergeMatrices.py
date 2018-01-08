cd912498f68188f7e28e3fc545b6078fa06fa55f
Marcely Zanon Boito <mzboito@gmail.com>
2018-01-08 16:05:22 +0100

    re-writing mergeMatrices

--- matrices/mergeMatrices.py
+++ matrices/mergeMatrices.py
@@ -85,7 +85,7 @@ def find_matrices(path_folders, folders, files_dict, file_name):
 	else:
 		size = len(matrices[0])
 		for i in range(1, len(folders)):
-			print matrices[i][0]
+			#print matrices[i][0]
 			if len(matrices[i]) != size:
 				print "PROBLEM WITH MATRIX SIZE: " + file_name + " index: " + str(i) + "\n"
 				sys.exit(1)
