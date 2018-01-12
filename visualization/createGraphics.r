ace6bb2d98dea35421dcb4e83af71d3295648134
Marcely Zanon Boito <mzboito@gmail.com>
2018-01-12 15:09:02 +0100

    cleaning

--- visualization/createGraphics.r
+++ visualization/createGraphics.r
@@ -5,11 +5,11 @@ library("ggplot2")
 
 data = read.csv(args[1],header=TRUE,sep="\t")
 
-print(data$dev_loss)
+#print(data$dev_loss)
 
 
 lossPlot  = ggplot(data=data, aes(x = data$step)) + geom_line(aes(y=data$dev_loss, color = "Dev")) +
-    geom_line(aes(y=data$train_loss,color= "Train")) + labs(title = "LOSS BEHAVIOR",x="Steps",y="Loss") + 
+    geom_line(aes(y=data$train_loss,color= "Train")) + labs(title = "LOSS BEHAVIOR",x="Steps",y="Loss", color = "set") + 
     scale_x_continuous(breaks=seq(min(data$step), max(data$step), by = min(data$step)*6), limits = c(0,max(data$step))) 
 
 
@@ -17,7 +17,7 @@ ggsave(file= paste(args[2],"lossPlot.png"), width = 7.0, height = 3.5)
 
 
 scoresPlot = ggplot(data=data, aes(x = data$step)) + geom_line(aes(y=data$dev_bleu, color = "Dev")) +
-geom_line(aes(y=data$train_bleu,color= "Train")) + labs(title = "BLEU SCORE",x="Steps",y="BLEU") + 
+geom_line(aes(y=data$train_bleu,color= "Train")) + labs(title = "BLEU SCORE",x="Steps",y="BLEU", color = "set") + 
   scale_x_continuous(breaks=seq(min(data$step), max(data$step), by = min(data$step)*6), limits = c(0,max(data$step))) 
 
 
