eb23f3f71ca13e412a94767d802f1a8f7bb37604
Marcely Zanon Boito <mzboito@gmail.com>
2018-01-12 17:34:02 +0100

    proportional baseline

--- alignment/baseline_corpus.py
+++ alignment/baseline_corpus.py
@@ -8,14 +8,14 @@ import glob
 #PH = "a0 a1 a19 a20 a25 a14 a45 a9 a9 a18 a33 a9 a27 a26 a7 a2 a30 a43 a31 a15 a13 a28 a10 a37 a31 a34 a35 a12 a42 a41 a35 a38 a12 a45 a29 a30 a41 a32 a37 a28 a7 a10 a34 a42 a3 a9 a39 a44 a6"
 #FR = "je te remercie pour le bien que tu m&apos; as fait"
 
-def segment(ph, fr):
+def segment(ph, target):
     #create two lists
     ph_list = ph.strip().split(" ")
-    fr_list = fr.replace("&apos;","\'").strip().split(" ")
-    fr_list_size = len(fr_list)
+    target_list = target.replace("&apos;","\'").strip().split(" ")
+    target_list_size = len(target_list)
     #get proportion of number of phonemes per symbol
     ph_number = len(ph_list) #phones number
-    gr_number = len(fr.replace("&apos;","\'").strip().replace(" ","")) #graphemes number
+    gr_number = len(target.replace("&apos;","\'").strip().replace(" ","")) #graphemes number
     #get proportion ph_number / gr_number
     ph_per_grapheme = int(math.floor(ph_number/float(gr_number)))
     #print ph_number, gr_number, ph_per_grapheme, ph_per_grapheme * gr_number
@@ -23,14 +23,14 @@ def segment(ph, fr):
     rest = ph_number - ph_per_grapheme*gr_number
     ph_per_word = 0
     if(rest > 0): #if we cannot separate in equal parts
-        fr_words = len(fr.split(" "))
-        #print fr_words, rest, rest > fr_words, rest/fr_words
-        if(rest >= fr_words):
-            ph_per_word = int(rest/fr_words) #if we have a remain, adds ph per word
+        target_words = len(target.split(" "))
+        #print target_words, rest, rest > target_words, rest/target_words
+        if(rest >= target_words):
+            ph_per_word = int(rest/target_words) #if we have a remain, adds ph per word
     #print ph_per_word
     index = 0
     segmentation = ""
-    for word in fr_list:
+    for word in target_list:
         w_length  = len(word)
         this_ph = (ph_per_grapheme * w_length) + ph_per_word
         #print word, w_length, this_ph
@@ -38,7 +38,7 @@ def segment(ph, fr):
         while(i < (this_ph + index) and i < ph_number):
             segmentation+=ph_list[i]
             i+=1
-        if(word != fr_list[fr_list_size-1]): #it it is not in the end
+        if(word != target_list[target_list_size-1]): #it it is not in the end
             segmentation += " "
         index += this_ph
     while(index < ph_number): #the rest
@@ -53,6 +53,10 @@ def write_file(path, sentences):
 
 def read_file(path):
     return [line for line in codecs.open(path,"r","uTF-8")]
+
+def print_usage():
+    print "Proportional baseline - corpus version \n"
+    print "arg1: unsegmented source (with blank spaces between the symbols)\narg2: aligned target translation\narg3: output file name\n"
 
 def main():
     source_list = read_file(sys.argv[1])
