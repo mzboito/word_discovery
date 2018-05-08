f2cc9d3153c4d6a4ebbb3091a07f95031a7438c4
mzboito <mzboito@gmail.com>
2018-05-08 21:49:24 +0200

    new evaluation script

---
+++ evaluation/boundary.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+
+class Sentences:
+    def __init__(self, gold, segmentation):
+        self.sentences = []
+        self.parse(gold, segmentation)
+    
+    def parse(self, gold, segmentation):
+        assert len(gold) == len(segmentation)
+        for i in range(0,len(gold)):
+            s = Sentence(gold[i], segmentation[i])
+            self.sentences.append(s)
+    
+    def boundary_score(self):
+        correct = 0
+        totalSeg = 0
+        totalGold = 0 
+        for sentence in self.sentences:
+            correct += sentence.boundary_score()
+            totalSeg += sentence.boundary_count(sentence.segmentation)
+            totalGold += sentence.boundary_count(sentence.gold)
+        return [correct, totalSeg, totalGold]    
+
+class Sentence:
+    def __init__(self, gold, segmentation):
+        self.gold = []
+        self.segmentation = [] 
+        self.parse(gold,segmentation)
+    
+    def parse(self, s1, s2):
+        s1_unseg = s1.replace(" ","")
+        s2_unseg = s2.replace(" ","")
+        assert len(s1_unseg) == len(s2_unseg)
+
+        self.gold = self.create_list(s1, s1_unseg)
+        self.segmentation = self.create_list(s2, s2_unseg)
+        self.create_alignment() 
+        
+    def create_list(self, s1, s1_unseg):
+        index = 0
+        order = 1
+        s1_list = []
+        for i in range(0,len(s1_unseg)):
+            assert s1_unseg[i] == s1[index]
+            if index +1 < len(s1) and s1[index+1] == " ": #if the next thing is a boundary
+                t = Token(s1_unseg[i], True, order)
+                order+=1
+                index+=2 #keeps aligned
+            else:
+                t = Token(s1_unseg[i], False, order)
+                s1_list.append(t)
+                index+=1
+        return s1_list
+    
+    def create_alignment(self):
+        for i in range(0,len(self.gold)):
+            self.gold[i].alignedToken = self.segmentation[i]
+            self.segmentation[i].alignedToken = self.gold[i]
+
+    def boundary_score(self):
+        score = 0
+        for i in range(0,len(self.gold)):
+            if self.gold[i].boundary and self.segmentation[i].boundary:
+                score+=1
+        return score
+    
+    def boundary_count(self, s1):
+        score = 0
+        for i in range(0, len(s1)):
+            if s1[i].boundary:
+                score+=1
+        return score
+
+class Token:
+    def __init__(self, token, boundary, order):
+        self.token = token
+        self.alignedToken = None #this receives a pointer (not being used for the moment)
+        self.boundary = boundary #boolean
+        self.order = order #id for finding the tokens (e.g. word1 = 1, word2 = 2)
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ evaluation/evaluation.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import codecs
+import argparse
+import itertools
+from boundary import Sentences, Sentence, Token
+
+def read_file(inputPath):
+    return [line.strip("\n") for line in codecs.open(inputPath, "r", "UTF-8")]
+
+def write_file(outputPath, scores):
+    types = scores[0]
+    tokens = scores[1]
+    boundaries = scores[2]
+    with open(outputPath, "w") as outputFile:
+        outputFile.write("TYPES\n")
+        outputFile.write("-"*20 + "\n")
+        outputFile.write("%2.2f\t%2.2f\t%2.2f\n\n" % (types[0], types[1], types[2]))
+        outputFile.write("TOKENS\n")
+        outputFile.write("-"*20 + "\n")
+        outputFile.write("%2.2f\t%2.2f\t%2.2f\n\n" % (tokens[0], tokens[1], tokens[2]))
+        outputFile.write("BOUNDARIES\n")
+        outputFile.write("-"*20 + "\n")
+        outputFile.write("%2.2f\t%2.2f\t%2.2f\n\n" % (boundaries[0], boundaries[1], boundaries[2]))
+
+def recall(correct, sizeGold):
+    return (float(correct) / sizeGold) * 100
+
+def precision(correct, totalFound):
+    return (float(correct) / totalFound) * 100
+
+def fscore(recall, precision):
+    return (2.0*recall*precision / (precision + recall))
+
+def score(correct, sizeSeg, sizeGold):
+    p = precision(correct, sizeSeg)
+    r = recall(correct, sizeGold)
+    return [p, r, fscore(r, p)]
+
+def get_tokens_score(segmentation, gold):
+    trueTokens = gold.strip("\n").split(" ")
+    segTokens = segmentation.strip("\n").split(" ")
+    score = 0
+    for token in segTokens: #for each generated token
+        if token in trueTokens: #check if it is on the true tokens list
+            trueTokens.remove(token) #avoid multiples matches for the same token 
+            score += 1
+    return score
+
+def get_tokens(set1):
+    tokens = [line.strip("\n").split(" ") for line in set1]
+    return list(itertools.chain.from_iterable(tokens))
+
+def get_types(set1):
+    tokens = get_tokens(set1)
+    return list(set(tokens)) #remove dups
+
+def count_boundaries(set1):
+    count = 0
+    for line in set1:
+        count += line.count(" ")
+    return count
+
+def tokens_intersection(segmentation, gold): #Intersection on sentence level to avoid scoring right tokens in wrong places
+    correctly_segmented = 0
+    for i in range(0, len(gold)):
+        correctly_segmented += get_tokens_score(segmentation[i], gold[i]) #number of tokens found
+    return correctly_segmented
+
+def types_intersection(segmentation, gold):
+    return len([t for t in segmentation if t in gold])
+
+def tokens_eval(segmentation, gold):
+    tokensGold = len(get_tokens(gold))
+    tokensSeg = len(get_tokens(segmentation))
+    correctTokens = tokens_intersection(segmentation, gold) #INTERSECTION ON SENTENCE LEVEL! 
+    return score(correctTokens, tokensSeg, tokensGold)
+
+def types_eval(segmentation, gold):
+    typesGold = get_types(gold)
+    typesSeg = get_types(segmentation)
+    correctTypes = types_intersection(typesSeg, typesGold)
+    return score(correctTypes, typesSeg, typesGold)
+
+def boundary_eval(segmentation, gold):
+    s = Sentences(gold,segmentation)
+    correctBoundaries, boundariesSeg, boundariesGold = s.boundary_score()
+    return score(correctBoundaries, boundariesSeg, boundariesGold)
+
+def evaluate(segmentation, gold):
+    types_score = types_eval(segmentation,gold)
+    tokens_score = types_eval(segmentation,gold)
+    boundaries_score = types_eval(segmentation,gold)
+    return [types_score, tokens_score, boundaries_score]
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--segmentation', type=str, nargs='?', help='path for generated segmentation')
+    parser.add_argument('--gold', type=str, nargs='?', help='path for gold standard file')
+    parser.add_argument('--output', type=str, nargs='?', help='name of the output file')
+    args = parser.parse_args()
+
+    if len(sys.argv) < 3:
+        parser.print_help()
+        sys.exit(1)
+    
+    segmentation = read_file(args.segmentation)
+    gold = read_file(args.gold)
+    assert len(segmentation) == len(gold)
+    scores = evaluate(segmentation, gold)
+    write_file(args.output, scores)
+
+if __name__ == '__main__':
+    main()
