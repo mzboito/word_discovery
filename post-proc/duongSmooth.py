dbc792b0e235eb676796729ee484a786d596b457
mzboito <mzboito@gmail.com>
2018-05-11 02:50:30 +0200

    duong debug

--- post-proc/duongSmooth.py
+++ post-proc/duongSmooth.py
@@ -27,11 +27,11 @@ def smooth(paths, target, outputPath):
 				if len(matrix[line]) == 2:
 					pass
 				elif column == 1: #first line
-					s_matrix[line][column] = str((float(matrix[line][column]) + float(matrix[line][column+1]))/2)
+					s_matrix[line][column] = (float(matrix[line][column]) + float(matrix[line][column+1]))/2
 				elif matrix[line][column] == matrix[line][-1]: #last line
-					s_matrix[line][column] = str((float(matrix[line][column]) + float(matrix[line][column-1]))/2)
+					s_matrix[line][column] = (float(matrix[line][column]) + float(matrix[line][column-1]))/2
 				else:
-					s_matrix[line][column] = str((float(matrix[line][column-1]) + float(matrix[line][column]) + float(matrix[line][column+1]))/3)
+					s_matrix[line][column] = (float(matrix[line][column-1]) + float(matrix[line][column]) + float(matrix[line][column+1]))/3
 		write_output(s_matrix, outputPath + filem.split("/")[-1])
 
 def main():
