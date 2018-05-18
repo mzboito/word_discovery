431f3d9715379cd1b994ca7032cc3995590fc5a1
Marcely Zanon-boito <mzboito@gmail.com>
2018-05-18 16:35:23 +0200

    fix 1 clean_dpseg

--- utils/clean_dpseg.py
+++ utils/clean_dpseg.py
@@ -2,15 +2,15 @@ import codecs
 import sys
 
 
-dpseg_out = [line for line in codecs.open(sys.argv[1], "r", "UTF-8")]
+dpseg_out = [line for line in codecs.open(sys.argv[1], "r", "UTF-8", errors="replace")]
 
 
 flag = False
 with codecs.open(sys.argv[2], "w", "UTF-8") as outputFile:
     for i in range(len(dpseg_out)):
-        if "_nstrings=xxx" in line[i]:
+        if "_nstrings=xxx" in dpseg_out[i]:
             break
         if flag:
-            outputFile.write(line)
-        if "State:" in line[i]:
+            outputFile.write(dpseg_out[i])
+        if "State:" in dpseg_out[i]:
             flag = True
