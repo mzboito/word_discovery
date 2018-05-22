14ba52265601a02c63bb591c96fdafc7370ff775
mzboito <mzboito@gmail.com>
2018-05-22 19:20:29 +0200

    add unseg part for split

--- utils/create_corpus_split.py
+++ utils/create_corpus_split.py
@@ -3,7 +3,11 @@ import codecs
 from random import shuffle
 
 PERCENTAGE = 10
-SUFFIXES = ["id", "gold", "it"]
+SUFFIXES = ["id", "gold", "it", "gr"]
+
+
+def clean(line):
+    return line.replace("\\","").replace("\'", "").replace("-","").replace("[","").replace(".","").replace("]","").replace("`","").replace("#","").replace("  "," ")
 
 def write_file(output_path, l_range, l_lists, index):
     with codecs.open(output_path + SUFFIXES[index], "w", "UTF-8") as output_path:
@@ -15,6 +19,15 @@ def write_files(output_path, l_range, l_lists):
     for index in range(len(SUFFIXES)):
         write_file(output_path, l_range, l_lists, index)
 
+def unseg(lang):
+    unseg = []
+    for sentence in lang:
+        line = sentence.replace(" ","").strip()
+        new_sentence = line[0]
+        for i in range(1,len(line)):
+            new_sentence += " " + line[i]
+        unseg.append(new_sentence + "\n")
+    return unseg
     
 def main():
     id_path = sys.argv[1]
@@ -25,14 +38,15 @@ def main():
         output_path += '/'
 
     ids = [line for line in codecs.open(id_path,"r","UTF-8")]
-    lang1 = [line for line in codecs.open(lang1_path,"r","UTF-8")]
-    lang2 = [line for line in codecs.open(lang2_path,"r","UTF-8")]
+    lang1 = [clean(line) for line in codecs.open(lang1_path,"r","UTF-8")]
+    lang2 = [clean(line) for line in codecs.open(lang2_path,"r","UTF-8")]
+    lang1_unseg = unseg(lang1)
 
-    assert len(ids) == len(lang1) and len(lang1) == len(lang2) 
+    assert len(ids) == len(lang1) and len(lang1) == len(lang2) and len(lang2) == len(lang1_unseg)
 
     parallel_list = []
     for index in range(len(ids)):
-        parallel_list.append( [ids[index], lang1[index], lang2[index]] )
+        parallel_list.append( [ids[index], lang1[index], lang2[index]], lang1_unseg[index] )
     
     shuffle(parallel_list)
 
