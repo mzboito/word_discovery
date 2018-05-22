b3066f3b01fd84eef43a1e838d6b51eb7cb4de06
mzboito <mzboito@gmail.com>
2018-05-22 15:21:37 +0200

    clean griko script

---
+++ utils/clean_griko.py
@@ -0,0 +1,12 @@
+import sys 
+import glob
+import codecs
+
+input_files = glob.glob(sys.argv[1] + "*")
+
+for f in input_files:
+    with codecs.open(f,"r","UTF-8") as inputFile:
+        with codecs.open(f + ".clean","r") as outputFile:
+            for line in inputFile:
+                line = line.replace("\\","").replace("\'", "").replace("-","")
+                outputFile.write(line)
