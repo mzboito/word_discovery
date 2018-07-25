08f0a7e462abefa5fe7ee485de7a8f0c565d248b
mzboito <mzboito@gmail.com>
2018-07-25 15:56:29 +0200

    update segmentation_stats

--- evaluation/segmentation_stats.py
+++ evaluation/segmentation_stats.py
@@ -5,9 +5,10 @@ import codecs
 from itertools import chain
 
 class Stats:
-    def __init__(self, path, name):
-        self.name = name
-        self.sentences = self.read_file(path)
+    def __init__(self, path):
+        self.path = path
+        self.name = self.path.split("/")[-1]
+        self.sentences = self.read_file()
         self.tokens = self.get_tokens()
         self.types = self.get_types()
         self.num_tokens = len(self.tokens)
@@ -18,8 +19,8 @@ class Stats:
         self.long = self.get_f(max)
         self.words_dict = self.get_frequency_count()
 
-    def read_file(self, path):
-        return [line.strip().split(" ") for line in codecs.open(path,"r","UTF-8")]
+    def read_file(self):
+        return [line.strip().split(" ") for line in codecs.open(self.path,"r","UTF-8")]
 
     def get_tokens(self):
         return list(chain.from_iterable(self.sentences))
@@ -56,30 +57,34 @@ class Stats:
                 words_dict[token] = 1
         return words_dict
 
-def write_stats(path, seg, gold):
-    with open(path, "w") as outputFile:
+def write_stats(segmentation):
+    with open(segmentation.path + ".stats", "w") as outputFile:
         outputFile.write("\t".join(["","num_tokens", "vocab_size", "avg_word_length", "avg_tokens_per_sentence", "shortest_word", "longest_word"]) + "\n")
-        for stats in [seg,gold]:
-            outputFile.write("\t".join([str(stats.name), str(stats.num_tokens), str(stats.vocab_size), str(stats.avg_word_length), str(stats.avg_tokens_per_sentence),str(len(stats.short)), str(len(stats.long))]) + "\n")
-        correct_types = set(gold.types).intersection(seg.types)
-        outputFile.write("Vocabulary Intersection: " + str(len(correct_types)) + "\n")
+        outputFile.write("\t".join([str(segmentation.name), str(segmentation.num_tokens), str(segmentation.vocab_size), str(segmentation.avg_word_length), str(segmentation.avg_tokens_per_sentence),str(len(segmentation.short)), str(len(segmentation.long))]) + "\n")
 
-def write_vocab(path, stats):
-    with codecs.open(path,"w", "UTF-8") as outputFile:
-        for key, value in sorted(stats.words_dict.iteritems(), key=lambda (k,v): (v,k), reverse=True):
-            outputFile.write("\t".join([key, str(stats.words_dict[key])]) + "\n")
+def write_vocab(segmentation):
+    with codecs.open(segmentation.path +".vocab","w", "UTF-8") as outputFile:
+        for key, value in sorted(segmentation.words_dict.iteritems(), key=lambda (k,v): (v,k), reverse=True):
+            outputFile.write("\t".join([key, str(segmentation.words_dict[key])]) + "\n")
 
 def main():
-    if len(sys.argv) < 4:
-        print "USAGE:\n python segmentation_stats.py <segmentation> <gold-standard> <output-folder>\n"
+    if len(sys.argv) < 3:
+        print "USAGE:\n python segmentation_stats.py <list of files>\n"
         exit(1)
-    stats_folder = sys.argv[3]
-    generated = Stats(sys.argv[1],"generated")
-    gold = Stats(sys.argv[2], "gold_standard")
-    write_stats(stats_folder + "/" + sys.argv[1].split("/")[-1] +".segmentation_stats", generated, gold)
-    #generates vocab file
-    write_vocab(stats_folder + "/" + sys.argv[1].split("/")[-1] + ".vocab", generated)
-    write_vocab(stats_folder + "/" + sys.argv[2].split("/")[-1] + ".vocab", gold)
+    
+    segmentations = [Stats(file_path) for file_path in sys.argv[1:]]
+    for segmentation in segmentations:
+        write_stats(segmentation)
+        write_vocab(segmentation)
+    
+    #stats_folder = sys.argv[3]
+    #generated = Stats(sys.argv[1],"generated")
+    #gold = Stats(sys.argv[2], "gold_standard")
+            #for stats in [seg,gold]:
+        #correct_types = set(gold.types).intersection(seg.types)
+        #outputFile.write("Vocabulary Intersection: " + str(len(correct_types)) + "\n")
+    #write_stats(stats_folder + "/" + sys.argv[1].split("/")[-1] +".segmentation_stats", generated, gold)
+    #write_vocab(stats_folder + "/" + sys.argv[2].split("/")[-1] + ".vocab", gold)
 
 if __name__ == '__main__':
     main()
