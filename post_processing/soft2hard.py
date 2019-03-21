0bec6830818d81d3468a87b276053432ca0102bb
Marcely Zanon Boito <mzboito@gmail.com>
2019-03-21 12:07:40 +0100

    soft2hard type study

--- post_processing/soft2hard.py
+++ post_processing/soft2hard.py
@@ -10,12 +10,15 @@ def get_soft2hard_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('--matrices-prefix', type=str, nargs='?', help='matrices prefix')
     parser.add_argument('--output-file', type=str, nargs='?', help='name for the output name')
+    #This target option is fixed because I do not experiment in the other direction anymore
     parser.add_argument('target',type=bool, default=True, nargs='?', help='default considers that the source is to segment, include this option to segment the target')
     parser.add_argument('--individual-files', type=str, nargs='?', help='list of names for generating individual files')
     parser.add_argument('--output-folder', type=str, nargs='?', help='folder for storing individual files')
     parser.add_argument('--translation', default=False, action='store_true', help='Creates a parallel file with the generated translation. It requires a suffix')
+    #merge this two parameters in the future
     parser.add_argument('--transformer', default=False, action='store_true', help='set for transformer path getter')
     parser.add_argument('--pervasive', default=False, action='store_true', help='set for pervasive path getter')
+    parser.add_argument('--type-entropy', default=False, action='store_true', help='computes the average entropy for each discovered type')
     return parser
 
 def get_path(number, paths, transformer=False, pervasive=False):
@@ -28,24 +31,23 @@ def get_path(number, paths, transformer=False, pervasive=False):
         for path in paths:
             if "." + str(number) + "." in path:
                 return path
-    #print(number)
     raise Exception("Path not found")
 
 def get_max_prob_col(line, sentenceMatrix):
-    maxValue = float(sentenceMatrix[line][1]) #start from the first line after the characters
     col = 1
+    maxValue = float(sentenceMatrix[line][col]) #start from the first line after the characters
     for i in range(2, len(sentenceMatrix[line])):
         if maxValue < float(sentenceMatrix[line][i]):
             col = i
             maxValue = float(sentenceMatrix[line][i])
     return col
 
-def segment(filePath, target, translation):
-    matrix = utils.read_matrix_file(filePath)
+def segment(file_path, target, translation):
+    matrix = utils.read_matrix_file(file_path)
     if translation:
         return segment_target_translation(matrix, target)
     else:
-        return segment_target(matrix, target),""
+        return segment_target(matrix, target), ""
 
 def segment_target(matrix, target):
     if not target:
@@ -102,60 +104,81 @@ def segment_target_translation(matrix, target):
     finalString = utils.clean_line(" ".join(discovered_words))
     return finalString, " ".join(translation)
 
-def writeOutput(finalString, output, mode="w"):
+def write_output(finalString, output, mode="w"):
     with codecs.open(output, mode, "UTF-8") as outputFile:
         outputFile.write(finalString + "\n")
 
-def shift_left(segmentation):
-    '''new_segmentation = ""
-    space_flag = False
-    for i in range(len(segmentation)):
-        if space_flag: #shift left for the first character after the space
-            space_flag = False
-            new_segmentation += segmentation[i] + " "
-        elif segmentation[i] == " ": #holds the space
-            space_flag = True
+def types_entropy(f_path, dictionary):
+    matrix = utils.read_matrix_file(f_path)
+    index_list = []
+    discovered_tokens = []
+    last_col = -1
+    for i in range(1, len(matrix)): #for each line
+        col = get_max_prob_col(i, matrix) #find its alignment
+        if last_col: #first element
+            index_list.append([i])
+            finalString += matrix[i][0] #put the character in the beginning
+        elif lastCol == col: # if the current character and the last one are not separated
+            finalString += matrix[i][0]
+            index_list[-1].append(i)
         else:
-            new_segmentation += segmentation[i]'''
-    return segmentation
+            discovered_tokens.append(finalString)
+            finalString = matrix[i][0]
+            index_list.append([i])
+
+    print(discovered_tokens)
+    print(index_list)
+    print(segment_target(matrix, True))
+    exit(1)
+
+    return dictionary
+
+def write_dictionary(f_path):
+    pass
 
 def run(args):
     sentencesPaths = glob.glob(args.matrices_prefix+"*.txt") 
-    if args.output_file: #segmentation on a single file
-        outputPath = args.output_file
+
+    if args.type_entropy:
+        output_path = args.output_file
+        TYPES = dict()
         for index in range(1, len(sentencesPaths)+1):
-            filePath = get_path(index, sentencesPaths, transformer=args.transformer, pervasive=args.pervasive)
-            finalstr, translation = segment(filePath, args.target, args.translation)
-            if args.pervasive:
-                finalstr = shift_left(finalstr)
-            writeOutput(finalstr, outputPath, "a")
+            # get file
+            file_path = get_path(index, sentencesPaths, transformer=args.transformer, pervasive=args.pervasive)
+            #get dictionary, token
+            TYPES = types_entropy(file_path, TYPES)
+        write_dictionary(output_path + "/types_entropy.csv")
+
+
+    if args.output_file: #segmentation on a single file
+        output_path = args.output_file
+        for index in range(1, len(sentencesPaths)+1):
+            file_path = get_path(index, sentencesPaths, transformer=args.transformer, pervasive=args.pervasive)
+            finalstr, translation = segment(file_path, args.target, args.translation)
+            write_output(finalstr, output_path, "a")
             if args.translation:
-                writeOutput(translation, outputPath+args.translation,"a")
+                write_output(translation, output_path+args.translation,"a")
 
     if args.individual_files and args.output_folder: #segmentation in individual files (with ID)
         files_output_list = utils.read_file(args.individual_files)
         folder = args.output_folder if args.output_folder[-1] == '/' else args.output_folder + '/'
         assert len(files_output_list) == len(sentencesPaths)
         for index in range(1, len(sentencesPaths)+1):
-            filePath = get_path(index, sentencesPaths, transformer=args.transformer, pervasive=args.pervasive)
-            finalstr, translation = segment(filePath, args.target, args.translation)
-            if args.pervasive:
-                finalstr = shift_left(finalstr)
+            file_path = get_path(index, sentencesPaths, transformer=args.transformer, pervasive=args.pervasive)
+            finalstr, translation = segment(file_path, args.target, args.translation)
             file_name = files_output_list[index-1].split("/")[-1] + ".hs" #split(".")[:-1]) + ".hardseg"
-            writeOutput(finalstr, folder + file_name)
+            write_output(finalstr, folder + file_name)
             if args.translation:
-                writeOutput(translation, folder + file_name +args.translation)
+                write_output(translation, folder + file_name +args.translation)
 
     elif args.output_folder: #segmentation in individual files (without ID)
         folder = args.output_folder if args.output_folder[-1] == '/' else args.output_folder + '/'
         for sentencePath in sentencesPaths:
             finalstr, translation = segment(sentencePath, args.target, args.translation)
-            if args.pervasive:
-                finalstr = shift_left(finalstr)
             file_name = sentencePath.split("/")[-1] + ".hs" #split(".")[:-1]) + ".hardseg"
-            writeOutput(finalstr, folder + file_name)
+            write_output(finalstr, folder + file_name)
             if args.translation:
-                writeOutput(translation, folder + file_name +args.translation)
+                write_output(translation, folder + file_name +args.translation)
 
 
 if __name__ == "__main__":
